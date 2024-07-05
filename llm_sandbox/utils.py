7d655f08f3da492c3722f9f967e710a7d7381e1d
Duy Huynh <vndee.huynh@gmail.com>
2024-07-05 23:37:02 +0700

    refactor: remove unnecessary functions

--- llm_sandbox/utils.py
+++ llm_sandbox/utils.py
@@ -3,6 +3,7 @@ import docker.errors
 
 from typing import List
 from docker import DockerClient
+from docker.models.images import Image
 
 
 def image_exists(client: DockerClient, image: str) -> bool:
@@ -21,107 +22,24 @@ def image_exists(client: DockerClient, image: str) -> bool:
         raise e
 
 
-def pull_image(client: DockerClient, image: str) -> List[str]:
+def pull_image(client: DockerClient, image: str, tag: str = "latest") -> Image:
     """
     Pull a Docker image
     :param client: Docker client
     :param image: Docker image
+    :param tag: Tag for the image, default is "latest"
     """
-    try:
-        image = client.images.pull(image)
-        return image.tags
-    except Exception as e:
-        raise e
+    image = client.images.pull(image, tag)
+    return image
 
 
-def build_image(client: DockerClient, path: str, tag: str) -> List:
+def build_image(client: DockerClient, path: str | None = None, tag: str = "llm-sandbox", dockerfile: str | None = None) -> Image:
     """
     Build a Docker image
     :param client: Docker client
-    :param path: Path to the Dockerfile
+    :param path: Path to the directory containing the Dockerfile
     :param tag: Tag for the image
+    :param dockerfile: Path to the Dockerfile, default is None
     """
-    try:
-        return client.images.build(path=path, tag=tag)
-    except Exception as e:
-        raise e
-
-
-def create_container(client: DockerClient, image: str, name: str) -> str:
-    """
-    Create a Docker container
-    :param client: Docker client
-    :param image: Docker image
-    :param name: Name of the container
-    :return: Docker container id
-    """
-    try:
-        container = client.containers.create(image, name=name)
-        return container.id
-    except Exception as e:
-        raise e
-
-
-def start_container(client: DockerClient, container_id: str):
-    """
-    Start a Docker container
-    :param client: Docker client
-    :param container_id: Docker container id
-    """
-    try:
-        container = client.containers.get(container_id)
-        container.start()
-    except Exception as e:
-        raise e
-
-
-def stop_container(client: DockerClient, container_id: str):
-    """
-    Stop a Docker container
-    :param client: Docker client
-    :param container_id: Docker container id
-    """
-    try:
-        container = client.containers.get(container_id)
-        container.stop()
-    except Exception as e:
-        raise e
-
-
-def remove_container(client: DockerClient, container_id: str):
-    """
-    Remove a Docker container
-    :param client: Docker client
-    :param container_id: Docker container id
-    """
-    try:
-        container = client.containers.get(container_id)
-        container.remove()
-    except Exception as e:
-        raise e
-
-
-def remove_image(client: DockerClient, image: str):
-    """
-    Remove a Docker image
-    :param client: Docker client
-    :param image: Docker image
-    """
-    try:
-        client.images.remove(image)
-    except Exception as e:
-        raise e
-
-
-def get_container_logs(client: DockerClient, container_id: str) -> str:
-    """
-    Get the logs of a Docker container
-    :param client: Docker client
-    :param container_id: Docker container id
-    :return: Logs of the container
-    """
-    try:
-        container = client.containers.get(container_id)
-        return container.logs()
-    except Exception as e:
-        raise e
+    image = client.images.build(path=path, dockerfile=dockerfile, tag=tag)
+    return image[0]
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,6 @@
 import docker
 import unittest
-from unittest.mock import patch, MagicMock
-from llm_sandbox.utils import image_exists, pull_image, build_image, create_container, start_container, stop_container, remove_container, remove_image
+from llm_sandbox.utils import image_exists, pull_image, build_image
 
 
 class TestUtilities(unittest.TestCase):
@@ -14,11 +13,24 @@ class TestUtilities(unittest.TestCase):
         cls.container_id = 'test_container_id'
 
     def test_pull_image(self):
-        result = pull_image(self.client, self.image)
-        self.assertEqual(result, [f"{self.image}:{self.tag}"])
+        self.assertIsNotNone(pull_image(self.client, self.image, self.tag))
+        self.assertTrue(image_exists(self.client, f"{self.image}:{self.tag}"))
 
-    def test_image_exists(self):
-        self.assertTrue(image_exists(self.client, self.image))
+        self.client.images.remove(f"{self.image}:{self.tag}")
+        self.assertFalse(image_exists(self.client, f"{self.image}:{self.tag}"))
+
+    def test_build_image(self):
+        self.assertIsNotNone(build_image(self.client, "tests", tag="llm-sandbox"))
+        self.assertTrue(image_exists(self.client, "llm-sandbox:latest"))
+
+        self.client.images.remove("llm-sandbox:latest")
+        self.assertFalse(image_exists(self.client, "llm-sandbox:latest"))
+
+        self.assertIsNotNone(build_image(self.client, path="tests", dockerfile="busybox.Dockerfile", tag="llm-sandbox"))
+        self.assertTrue(image_exists(self.client, "llm-sandbox:latest"))
+
+        self.client.images.remove("llm-sandbox:latest")
+        self.assertFalse(image_exists(self.client, "llm-sandbox:latest"))
 
 
 if __name__ == '__main__':
