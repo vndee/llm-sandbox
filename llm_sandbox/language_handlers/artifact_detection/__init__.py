74d4dff56887c5a062bf9fe57c7770364703e339
Duy Huynh <vndee.huynh@gmail.com>
2025-06-18 14:28:20 +0700

    Update artifact detection init to include R plot detection

--- llm_sandbox/language_handlers/artifact_detection/__init__.py
+++ llm_sandbox/language_handlers/artifact_detection/__init__.py
@@ -1,5 +1,6 @@
 """Artifact detection for different languages."""
 
 from .python_plot_detection import PYTHON_PLOT_DETECTION_CODE
+from .r_plot_detection import R_PLOT_DETECTION_CODE
 
-__all__ = ["PYTHON_PLOT_DETECTION_CODE"]
+__all__ = ["PYTHON_PLOT_DETECTION_CODE", "R_PLOT_DETECTION_CODE"]
